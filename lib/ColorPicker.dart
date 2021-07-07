4e513e8da0e5edeb64d8aaa92eabf9293c0be3fa
yulimlee <yulimlee@atoncorp.com>
2021-07-08 08:54:36 +0900

    - color grid 생성

--- lib/ColorPicker.dart
+++ lib/ColorPicker.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:leftover_flutter/style/colors.dart';
 import 'style/font.dart';
 
-void main(){}
+void main() {}
 
-class ColorPicker extends StatelessWidget{
+class ColorPicker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,14 +32,37 @@ class _ColorPickerPageState extends State<ColorPickerPage> {
     Navigator.of(context, rootNavigator: true).pop(context);
   }
 
+  List<Color> colorList = [
+    LeftOverColor.use_light_aquamarine,
+    LeftOverColor.use_manila,
+    LeftOverColor.use_pastel_purple,
+    LeftOverColor.use_sun_yellow,
+    LeftOverColor.use_pale_salmon,
+    LeftOverColor.use_light_blue,
+    LeftOverColor.use_pale_orange
+  ];
+
+  List<Container> _buildColorGridList() => List.generate(
+      7,
+      (index) => Container(
+            width: 45,
+            height: 45,
+            child: Icon(Icons.check, color: Colors.white),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: colorList[index],
+          )));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
           widget.title,
-          style:
-          const TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontFamily: LeftOverTextStyle.notoSans),
+          style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontFamily: LeftOverTextStyle.notoSans),
           textAlign: TextAlign.center,
         ),
         backgroundColor: Colors.white,
@@ -50,7 +74,16 @@ class _ColorPickerPageState extends State<ColorPickerPage> {
             onPressed: () => closeThisScreen()),
         centerTitle: true,
       ),
+      body: Center(
+        child: Container(
+          child: GridView.count(
+            crossAxisCount: 5,
+            childAspectRatio: 3/2,
+            padding: EdgeInsets.fromLTRB(35, 33, 21, 22),
+            children: _buildColorGridList()
+          ),
+        ),
+      ),
     );
   }
-
 }
