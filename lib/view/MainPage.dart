29c1bde1d3df502aec94618ec5f78a918451086f
yulimlee <yulimlee@atoncorp.com>
2021-12-13 17:27:54 +0900

    async 로 변경

--- lib/view/MainPage.dart
+++ lib/view/MainPage.dart
@@ -1,12 +1,53 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:leftover_flutter/data/DBHelper.dart';
+import 'package:leftover_flutter/data/TicketInfo.dart';
+import 'package:leftover_flutter/style/font.dart';
 import 'package:leftover_flutter/view/NewTicketPage.dart';
 import 'package:leftover_flutter/view/TicketsListPage.dart';
 import 'package:leftover_flutter/style/colors.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class MainPage extends StatelessWidget{
+class MainPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MainPage();
+}
+
+class _MainPage extends State<MainPage>{
+  List<TicketInfo> ticketList = [];
+
+  var ticketNameStyle = TextStyle(
+      color: LeftOverColor.text_black,
+      fontFamily: LeftOverTextStyle.notoSans,
+      fontWeight: FontWeight.bold,
+      fontSize: 18);
+
+  Widget _buildTicketEnrolledList(BuildContext context) {
+    if(ticketList.isEmpty){
+      return emptyText;
+    }else{
+      var list = List.generate(ticketList.length, (index) =>
+          Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                  child: Text(ticketList[index].name, style: ticketNameStyle)
+              ))
+          );
+      return list;
+    }
+  }
+
+  var emptyText = Expanded(
+      child: Align(
+          alignment: Alignment.center,
+          child: Text(
+            '등록된 정보가 없습니다.',
+            style: TextStyle(color: Colors.grey),
+          )));
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,44 +73,66 @@ class MainPage extends StatelessWidget{
         ],
         centerTitle: false,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            TableCalendar(
-              firstDay: DateTime.utc(2010, 10, 16),
-              lastDay: DateTime.utc(2030, 3, 14),
-              focusedDay: DateTime.now(),
-              availableCalendarFormats: const {
-                CalendarFormat.month: '월',
-                CalendarFormat.week: '주',
-              },
-              headerStyle:
-              HeaderStyle(formatButtonVisible: false, titleCentered: true),
-              locale: 'ko_kr',
-            ),
-            Expanded(
-                child: Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      '등록된 정보가 없습니다.',
-                      style: TextStyle(color: Colors.grey),
-                    )))
-          ],
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed:  () => {
-        Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => NewTicketPage()))
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => NewTicketPage()))
         },
         tooltip: 'Add',
         child: Icon(Icons.add),
         backgroundColor: Colors.white,
         foregroundColor: LeftOverColor.logo_purpley,
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      body: FutureBuilder(
+        future: _getTicketData(),
+       builder: (BuildContext context, AsyncSnapshot snapshot){
+          if(snapshot.hasData == false){
+            return Center(
+              child: Container(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }else{
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  TableCalendar(
+                    firstDay: DateTime.utc(2010, 10, 16),
+                    lastDay: DateTime.utc(2030, 3, 14),
+                    focusedDay: DateTime.now(),
+                    availableCalendarFormats: const {
+                      CalendarFormat.month: '월',
+                      CalendarFormat.week: '주',
+                    },
+                    headerStyle:
+                    HeaderStyle(formatButtonVisible: false, titleCentered: true),
+                    locale: 'ko_kr',
+                  ),
+                  Expanded(
+                      child: _buildTicketEnrolledList(context);
+                  )
+                ],
+              ),
+            );
+          }
+       },
+      ));
+  }
+
+
+  Future<void> _getTicketFromDB() async{
+    ticketList.clear();
+    var tickets = DBHelper().getAllTicket().then((value) => value.forEach((element) {
+      ticketList.add(element);
+    }));
+    return tickets;
+  }
+
+  Future<int> _getTicketData() async {
+    var result = await _getTicketFromDB();
+    return ticketList.length;
   }
 
 }
